7d372848005c1127cbf1208949e98ec9daba5755
ryandomasta <diamondcrafters@outlook.com>
2016-12-17 10:33:46 -0700

    autotime

---
+++ autotime.lua
@@ -0,0 +1,605 @@
+print("kiddietron's 'Day - Night cycle' Loaded")
+
+while true do
+
+	wait(5)	
+	game.Lighting.TimeOfDay = "1:00:00"
+	wait(2)
+	game.Lighting.TimeOfDay = "1:01:00"
+	wait(2)
+	game.Lighting.TimeOfDay = "1:02:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "1:03:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "1:04:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "1:05:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "1:06:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "1:07:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "1:08:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "1:09:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "1:12:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "1:14:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "1:16:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "1:18:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "1:20:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "1:22:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "1:24:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "1:26:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "1:28:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "1:30:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "1:34:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "1:38:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "1:40:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "1:43:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "1:48:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "1:52:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "1:58:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "2:00:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "2:05:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "2:08:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "2:12:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "2:16:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "2:20:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "2:24:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "2:30:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "2:35:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "2:39:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "2:43:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "2:48:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "2:53:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "2:59:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "3:02:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "3:07:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "3:10:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "3:15:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "3:20:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "3:25:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "3:30:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "3:35:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "3:39:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "3:43:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "3:49:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "3:53:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "3:58:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "4:00:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "4:05:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "4:09:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "4:14:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "4:19:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "4:23:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "4:28:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "4:34:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "4:39:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "4:43:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "4:47:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "4:50:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "4:54:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "4:58:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "5:04:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "5:09:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "5:14:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "5:19:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "5:24:00"	
+	wait(1)
+	game.Lighting.TimeOfDay = "5:28:00"	
+	wait(1)
+	game.Lighting.TimeOfDay = "5:34:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "5:38:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "5:42:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "5:47:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "5:52:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "5:57:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "6:00:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "6:05:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "6:10:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "6:15:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "6:20:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "6:25:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "6:30:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "6:35:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "6:40:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "6:45:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "6:50:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "6:55:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "7:00:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "7:05:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "7:10:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "7:15:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "7:20:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "7:25:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "7:30:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "7:35:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "7:40:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "7:45:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "7:50:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "7:55:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "8:00:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "8:05:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "8:10:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "8:15:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "8:20:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "8:25:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "8:30:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "8:35:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "8:40:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "8:45:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "8:50:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "8:55:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "9:00:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "9:05:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "9:10:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "9:15:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "9:20:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "9:25:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "9:30:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "9:35:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "9:40:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "9:45:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "9:50:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "9:55:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "10:00:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "10:05:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "10:10:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "10:15:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "10:20:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "10:25:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "10:30:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "10:35:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "10:40:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "10:45:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "10:50:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "10:55:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "11:00:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "11:05:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "11:10:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "11:15:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "11:20:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "11:25:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "11:30:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "11:35:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "11:40:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "11:45:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "11:50:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "11:55:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "12:00:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "12:05:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "12:10:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "12:15:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "12:20:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "12:25:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "12:30:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "12:35:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "12:40:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "12:45:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "12:50:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "12:55:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "13:00:00"	
+	wait(2)
+ 	game.Lighting.TimeOfDay = "13:05:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "13:10:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "13:15:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "13:20:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "13:25:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "13:30:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "13:35:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "13:40:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "13:45:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "13:50:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "13:55:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "14:00:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "14:05:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "14:10:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "14:15:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "14:20:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "14:25:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "14:30:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "14:35:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "14:40:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "14:45:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "14:50:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "14:55:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "15:00:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "15:05:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "15:10:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "15:15:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "15:20:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "15:25:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "15:30:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "15:35:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "15:40:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "15:45:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "15:50:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "15:55:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "16:00:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "16:05:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "16:10:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "16:15:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "16:20:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "16:25:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "16:30:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "16:35:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "16:40:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "16:45:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "16:50:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "16:55:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "17:00:00"	
+	wait(1)
+	game.Lighting.TimeOfDay = "17:05:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "17:10:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "17:15:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "17:20:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "17:25:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "17:30:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "17:35:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "17:40:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "17:45:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "17:50:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "17:55:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "18:00:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "18:05:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "18:10:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "18:15:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "18:20:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "18:25:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "18:30:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "18:35:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "18:40:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "18:45:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "18:50:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "18:55:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "19:00:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "19:05:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "19:10:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "19:15:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "19:20:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "19:25:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "19:30:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "19:35:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "19:40:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "19:45:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "19:50:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "19:55:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "20:00:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "20:05:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "20:10:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "20:15:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "20:20:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "20:25:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "20:30:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "20:35:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "20:40:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "20:45:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "20:50:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "20:55:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "21:00:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "21:05:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "21:10:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "21:15:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "21:20:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "21:25:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "21:30:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "21:35:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "21:40:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "21:45:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "21:50:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "21:55:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "22:00:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "22:05:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "22:10:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "22:15:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "22:20:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "22:25:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "22:30:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "22:35:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "22:40:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "22:45:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "22:50:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "22:55:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "23:00:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "23:05:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "23:10:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "23:15:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "23:20:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "23:25:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "23:30:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "23:35:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "23:40:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "23:45:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "23:50:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "23:55:00"	
+	wait(2)
+	game.Lighting.TimeOfDay = "24:00:00"	
+	wait(2)
+
+
+
+end
+
+script.Parent.Touched:connect(onHit)
